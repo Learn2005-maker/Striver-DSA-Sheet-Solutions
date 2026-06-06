1b0fd1c3082d49fd94c4e9c64da584a83a19fcde
sujithkumar <sujith8179@gmail.com>
2026-06-06 19:41:43 +0530

    linkedpro

--- LinkedListProblems/findthePairswithSum.py
+++ LinkedListProblems/findthePairswithSum.py
@@ -59,4 +59,31 @@ head=findPairswithGivenSum(head,target)
 print(head)
 # Time Complexity = O(n²)
 # Space Complexity = O(1) excluding the output
-        
+
+
+
+def findTail(head):
+    temp=head
+    while temp.next:
+        temp=temp.next
+    return temp
+def findPairswithGivenSum(head,target):
+    left=head
+    right=findTail(head)
+    arr=[]
+    while left.data < right.data:
+        if left.data+right.data==target:
+            arr.append([left.data,right.data])
+            left=left.next
+            right=right.prev
+        elif left.data+right.data>target:
+            right=right.prev
+        else:
+            left=left.next
+    return arr
+target=5    
+head=findPairswithGivenSum(head,target)
+
+print(head)
+# Time Complexity = O(n)
+# Space Complexity = O(1) excluding the output        
