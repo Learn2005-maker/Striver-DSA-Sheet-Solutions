9ad88be6d673fe166999d3e9fbef3720413b7192
sujithkumar <sujith8179@gmail.com>
2026-06-22 21:37:55 +0530

    Timecomplex

--- RecursionPROBLEMS/CountSubsequenxes.py
+++ RecursionPROBLEMS/CountSubsequenxes.py
@@ -1,6 +1,30 @@
 # Time complexcity:O(N*2^N)
 # space complexity:O(N) - to store the powerset
 
+# This is main answer to count the number of subsequences whose sum is equal to k
+count=0
+def Countsubseqs(nums,st_index,curr_subset,k):
+    global count
+    if st_index>=len(nums):
+        if sum(curr_subset[:])==k:
+            count+=1    
+        return
+    curr_subset.append(nums[st_index])
+    Countsubseqs(nums,st_index+1,curr_subset,k)
+    curr_subset.pop()    
+    Countsubseqs(nums,st_index+1,curr_subset,k)
+nums = [4, 2,10,5,1,3]
+k = 5  
+
+Countsubseqs(nums,0,[],k) 
+print(count)
+
+
+
+
+
+
+# This is to count the number of subsequences in an array
 powerSet=[]
 count=0
 def Countsubseqs(nums,st_index,curr_subset):
