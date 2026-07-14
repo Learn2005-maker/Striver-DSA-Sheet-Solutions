6385cddacab444513d7f71704cadd4b5a9e4ce24
sujithkumar <sujith8179@gmail.com>
2026-07-14 11:42:05 +0530

    bits

---
+++ BitManipulationDSA/checkODDorEVEN.py
@@ -0,0 +1,8 @@
+n=8
+
+if n&1!=0:
+    print("odd")
+else:
+    print("even")
+# using bitwise AND operator to check if a number is odd or even. 
+# If the least significant bit (LSB) is 1, the number is odd; if it's 0, the number is even.
