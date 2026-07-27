af28798f3e65e8b2ff4a213e3bde484076ac868e
K sujithkumar <sujith8179@gmail.com>
2026-07-27 16:43:50 +0530

    stackusingqueue

--- StacksQueusProblems/EasyProblems/ImplementationofQueueUsingStack.py
+++ StacksQueusProblems/EasyProblems/ImplementationofQueueUsingStack.py
@@ -33,6 +33,50 @@ print("pop element",q.pop())
 q.display()
 print("Top Element",q.peek())
 print(q.empty())       
+
+
+
+
+class MyQueue:
+    def __init__(self):
+        self.s1=[]
+        self.s2=[]
+        
+    def push(self,x):
+        self.s1.append(x)
+        
+    def pop(self) :
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+        
+    def peek(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]      
+        
+    def  empty(self):
+        return len(self.s1)==0 and len(self.s2)==0
+        
+    def display(self):
+        print(self.s1[::-1])
+
+q=MyQueue()
+q.push(10)
+q.push(30)
+q.push(20)
+
+q.display()
+print("pop element",q.pop())
+q.display()
+print("Top Element",q.peek())
+print(q.empty() )       
+            
+# Time complexcity:Push O(1) , pop and top: O(n) not approxmiately O(n)   # 
+        
+        
             
         
         
