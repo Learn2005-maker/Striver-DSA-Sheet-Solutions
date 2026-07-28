fb56ee926b012fbb7678e275fb891cca0be50a0e
K sujithkumar <sujith8179@gmail.com>
2026-07-28 17:19:58 +0530

    stackusingqueue

---
+++ StacksQueusProblems/EasyProblems/validparenthese.py
@@ -0,0 +1,26 @@
+# Time complexcity:O(n)
+# spac complexcity:O(n)
+# Approach: Store opening brackets in Stack.
+def isValid(s):
+    n=len(s)
+    
+    stack=[]
+    
+    for i in range(n):
+        if s[i]=='(' or s[i]=='[' or s[i]  =='{':
+            stack.append(s[i])
+        else:
+            if len(stack)==0:
+                return False
+            ch=stack[-1]
+            stack.pop()
+            if s[i]==')' and ch=='(' or s[i]==']' and ch=='[' or s[i]=='}' and ch=='{':
+                continue
+            else:
+                return False
+    return len(stack )==0         
+            
+s="(){}{{{"            
+print(isValid(s)   )        
+        
+        
