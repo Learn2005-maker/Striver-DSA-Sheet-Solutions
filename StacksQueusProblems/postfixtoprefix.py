6f2561cbd36f94e6e322f7a5298a37fb3dd8b16c
K sujithkumar <sujith8179@gmail.com>
2026-08-01 11:34:13 +0530

    postfic

---
+++ StacksQueusProblems/postfixtoprefix.py
@@ -0,0 +1,17 @@
+def postfix_to_prefix(exp):
+    stack=[]
+    for ch in exp:
+        if ch.isalnum():
+            stack.append(ch)
+        else:
+            t1=stack.pop()
+            t2=stack.pop()
+            convertedstr=ch+t2+t1
+            stack.append(convertedstr)
+    return stack[-1]
+    
+exp = "AB+CD-*"
+print(f"Postfix: {exp}")
+print(f"Prefix:  {postfix_to_prefix(exp)}")
+    
+    
