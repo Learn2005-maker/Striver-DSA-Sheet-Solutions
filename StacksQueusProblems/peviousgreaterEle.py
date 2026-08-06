acf139afa18faad507f25e898ad710753069f12d
K sujithkumar <sujith8179@gmail.com>
2026-08-06 16:45:51 +0530

    daa1stpratical

---
+++ StacksQueusProblems/peviousgreaterEle.py
@@ -0,0 +1,11 @@
+nums=[5,7,9,10,3,4]
+n=len(nums)
+stack=[]
+nge=[-1]*n
+for  i in range(n):
+    while stack and stack[-1]>=nums[i]:
+        stack.pop()
+    if stack:
+        nge[i]=stack[-1]
+    stack.append(nums[i])
+print(nge)    
