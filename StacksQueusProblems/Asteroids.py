9685a9a7aee3f3bf57eaa19517fbf784941bfa84
Sujith Chanti <sujith8179@gmail.com>
2026-08-07 17:18:13 +0530

    asteroids

---
+++ StacksQueusProblems/Asteroids.py
@@ -0,0 +1,18 @@
+asteroids=[8,-8]
+n=len(asteroids)
+stack=[]
+for i in range(n):
+    if asteroids[i]>0:
+        stack.append(asteroids[i])        
+    else:
+        while stack and stack[-1]>0 and stack[-1]<abs(asteroids[i]):
+            stack.pop()
+        if not stack or stack[-1]<0:
+            stack.append(asteroids[i])
+        elif stack and stack[-1]==abs(asteroids[i]) :
+            stack.pop()
+    
+        
+print(stack)    
+# Time complexcity: O(2n)
+# Space complexcity:O(n)
--- StacksQueusProblems/TrappingRainwater.py
+++ StacksQueusProblems/TrappingRainwater.py
@@ -24,3 +24,31 @@ res=trap(height)
 print(res)
 # Time complexcity:O(3n)
 # Space complexcity:O(2n)
+
+
+
+# You can also do with O(n) space and O(n) time complexcity by using only suffix_max array and left_max variable.
+def trap(height):
+    n=len(height)
+    total=0
+    # prefix_max=[0]*n
+    suffix_max=[0]*n
+    # prefix_max[0]=height[0]
+    # for i in range(1,n):
+        
+    #     prefix_max[i]=max(prefix_max[i-1],height[i])
+    suffix_max[n-1]=height[n-1]
+    for i in range(n-2,-1,-1):
+        suffix_max[i]=max(suffix_max[i+1],height[i])
+    left_max=0
+    for i in range(n):
+        left_max=max(left_max,height[i])
+        right_max=suffix_max[i]
+        if height[i]<left_max and height[i]<right_max:
+            total=total+min(left_max,right_max)-height[i]
+    return total
+
+height=[0,1,0,2,1,0,1,3,2,1,2,1]
+
+res=trap(height)
+print(res)
