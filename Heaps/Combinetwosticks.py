d981dffca8b727924658df611369ef30c0346b76
Sujith Chanti <sujith8179@gmail.com>
2026-09-02 10:33:46 +0530

    sticks

---
+++ Heaps/Combinetwosticks.py
@@ -0,0 +1,19 @@
+sticks = [2,4,3]
+n=len(sticks)
+
+import heapq
+heap=[]
+
+total=0
+for i in range(n):
+    heapq.heappush(heap,sticks[i])
+
+while len(heap)>1:
+    min1=heapq.heappop(heap)
+    min2=heapq.heappop(heap)
+    t=min1+min2
+    total+=t
+    heapq.heappush(heap,t)
+print(total)
+# T.c:O(nlogn)
+# S.c:O(n)
