c2215e08c5933f70c0977d864077aaabfb71c066
Sujith Chanti <sujith8179@gmail.com>
2026-09-04 23:59:41 +0530

    jump2

--- GreedyAlgorithms/jumpGame2.py
+++ GreedyAlgorithms/jumpGame2.py
@@ -15,3 +15,27 @@ print(jump2(0,0,nums))
 # time complexity: O(N^N) and it excecds TLE in leetcode
 
 
+nums = [2,3,1,4,1,1,1,2]
+n=len(nums)
+
+
+l=0
+r=0
+
+jumps=0
+# while is to check the farthest range. ( did you reach)   
+# Time complxity:O(n)
+while r<n-1:
+    farthest=0
+
+    for i in range(l,r+1):
+        farthest=max(i+nums[i],farthest)
+    l=r+1
+    r=farthest
+    jumps+=1
+
+print(jumps)
+
+
+
+
