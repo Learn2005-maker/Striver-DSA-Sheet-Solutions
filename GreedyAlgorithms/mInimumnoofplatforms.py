a4e555b0c29f3ada2c9e9fe25869037aedbcdf63
Sujith Chanti <sujith8179@gmail.com>
2026-09-09 18:59:32 +0530

    platofomrs

--- GreedyAlgorithms/mInimumnoofplatforms.py
+++ GreedyAlgorithms/mInimumnoofplatforms.py
@@ -19,3 +19,38 @@ Arrival = [900, 940, 950, 1100, 1500, 1800]
 Departure = [910, 1200, 1120, 1130, 1900, 2000]
 
 print("Minimum platforms needed:", findPlatform(Arrival, Departure))
+
+
+
+# Figure out maxmum no of intersections in extreme main solution
+
+
+def findPlatform(Arrival,Departure):
+  Arrival.sort()
+  Departure.sort()
+  n=len(Arrival)
+  maxCount=0
+
+  i=0
+  j=0
+  count=0
+  while i<n  and j<n:
+    if Arrival[i]<=Departure[j]:
+      count+=1
+      i+=1
+    else:
+      count-=1
+      j+=1
+    maxCount=max(maxCount,count)
+  return maxCount
+      
+    
+    
+  
+  
+Arrival = [900, 940, 950, 1100, 1500, 1800]
+Departure = [910, 1200, 1120, 1130, 1900, 2000]
+
+
+
+print(findPlatform(Arrival,Departure))
