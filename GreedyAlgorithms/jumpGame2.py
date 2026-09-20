4eb7cc54649541d5444f0bf045101f39ef1cdb33
Sujith Chanti <sujith8179@gmail.com>
2026-09-20 15:30:16 +0530

    knapsack

--- GreedyAlgorithms/jumpGame2.py
+++ GreedyAlgorithms/jumpGame2.py
@@ -1,9 +1,9 @@
-def jump2(index,jumps):
-  if index>=len(nums)-1:
+def jump2(index,jumps,arr):
+  if index>=len(arr)-1:
     return jumps
   minI=float("inf")
 
-  for i in range(1,nums[index]+1):
+  for i in range(1,arr[index]+1):
     minI=min(minI,jump2(index+i,jumps+1,nums))
   return minI
   
