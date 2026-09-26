d9bff30e200ef0190742c4b608f792fd8874f8fb
Sujith Chanti <sujith8179@gmail.com>
2026-09-26 11:11:11 +0530

    Diameter

---
+++ Trees/BasicEasyProblems/DiameterOfTree.py
@@ -0,0 +1,42 @@
+# Brute Force Approach
+# Time complxity: O (n^2)
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def Height(node):
+            if node is None:
+                return 0
+            
+            lh=Height(node.left)
+            rh=Height(node.right)
+            return 1+max(lh,rh)
+        if root is None:
+            return 0
+        maxi=0
+        lh=Height(root.left)
+        rh=Height(root.right)
+        currentD=lh+rh
+        leftD=self.diameterOfBinaryTree(root.left)
+        rightD=self.diameterOfBinaryTree(root.right)
+        return max(currentD,leftD,rightD)
+
+# Optimal Approach
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = [0]
+
+        def Height(node):
+            if node is None:
+                return 0
+
+            lh = Height(node.left)
+            rh = Height(node.right)
+
+            diameter[0] = max(diameter[0], lh + rh)
+
+            return 1 + max(lh, rh)
+
+        Height(root)
+        return diameter[0]
+    
+    
