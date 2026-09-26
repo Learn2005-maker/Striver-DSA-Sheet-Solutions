4a6babd0102d9099dbf5e4628cce1774a87c1bd2
Sujith Chanti <sujith8179@gmail.com>
2026-09-26 12:24:08 +0530

    diameter

---
+++ Trees/BasicEasyProblems/Hard/mAimumPAthSUm.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxPathSum( root):
+        maxi=[float("-inf")]
+
+        def maxPath(node):
+            if node is None:
+                return 0
+            
+            leftSum=max(0,maxPath(node.left))
+            rightSum=max(0,maxPath(node.right))
+            maxi[0]=max(maxi[0],node.val+leftSum+rightSum)
+            return node.val+max(leftSum,rightSum)
+        maxPath(root)
+        return maxi[0]
+        
